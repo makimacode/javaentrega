68865dd44b38910e583268503793d5cf96e46c17
matimastroti <mastroti31@gmail.com>
2022-10-25 21:05:56 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,36 @@
+//let nombreingresado = prompt("Ingrese su Nombre")
+//console.log("El usuario ingreso: " + nombreingresado)
+//alert("El usuario ingreso: "+ nombreingresado)
+
+//console.log(10>2)
+
+
+// let password = khali27
+
+// let InputUsser = Number(prompt("ingrese su clave"))
+//    // productos = prompt("ingrese un producto")
+    
+   // console.log("ciclo for: " + i);
+//}
+
+
+
+// let password = 27;
+// let InputUsser= Number(prompt("ingrese su clave"))
+// // let password = khali27
+// if(password==InputUsser) {
+//     alert("muy bien amor")
+// } else {
+//     alert("mal amor")
+// }
+
+let tiro= 1
+let apuntar= Number(prompt("Elija donde tirar 1 o 2"))
+if(tiro==apuntar) {
+    alert ("GOOOOL CAMPEONES")
+ } else
+ {alert 
+   ("ERRASTE Y PERDIERON")
+ };
+
+ 
